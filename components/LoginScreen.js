1da17f7ba52657502a46180c42656a3bc126c3be
plugin23 <xvalicek@stuba.sk>
2022-04-13 20:21:29 +0200

    components as functions, StackNavigation, BottomTabs implemented

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -2,94 +2,71 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack'
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput, Alert, ActivityIndicator, TouchableOpacity} from 'react-native';
 import { fetchAPI } from '../Api'
 
 const Stack = createStackNavigator()
 
-class LoginScreen extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            email: "",
-            password: "",
-            isLoading: false
-        }
-
-        this.handleUser = this.handleUser.bind(this)
-        this.handlePass = this.handlePass.bind(this)
-    }
-
-    handleUser = (text) => {
-        this.setState({ email: text })
-    }
-
-    handlePass = (pass) => {
-        this.setState({ password: pass })
-    }
-
-    login = () => {
-        if (this.state.email == "" || this.state.password == "") {
+const LoginScreen = (props) => {
+    
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+ 
+    const login = () => {
+        if (email == "" || password == "") {
             Alert.alert("Prázdne pole", "Prosím vyplňte obe polia", [{ text: "OK", onPress: () => { } }])
             return
         }
 
-        this.setState({
-            isLoading: true
-        })
+        setIsLoading(true)
 
         const bodyObject = {
-            email: this.state.email,
-            password: this.state.password
+            email: email,
+            password: password
         }
 
         fetchAPI('api/autobazar/users/login', 'POST', bodyObject).then(result => {
-            this.setState({
-                isLoading: false
-            })
+            setIsLoading(false)
+
             if (result.id) {
                 //alert(result)
-                this.props.loggedIn(result.id)
+                props.loggedIn(result.id)
             }
             else { //ak uzivatel zada zle heslo alebo meno
-                alert("Nesprávne údaje", "Údaje, ktoré ste zadali nie sú správne", [{ text: "OK", onPress: () => { } }])
+                Alert.alert("Nesprávne údaje", "Údaje, ktoré ste zadali nie sú správne", [{ text: "OK", onPress: () => { } }])
             }
         })
     }
-
-    register = () => {
-        this.props.showRegister()
-    }
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.logo}>Autobazár</Text>
-                <View style={styles.buttonMargin}>
-                    <TextInput style={styles.inputText} placeholder="Prihlasovacie meno" onChangeText={this.handleUser} />
-                    <View style={styles.separator} />
-                    <TextInput style={styles.inputText} placeholder="Heslo" onChangeText={this.handlePass} secureTextEntry={true} />
-                    <View style={styles.separator} />
-                    <TouchableOpacity
-                        onPress={this.login}
-                        style={styles.buttonStyle}>
-                        <Text style={styles.buttonText}>Prihlásiť sa</Text>
-                    </TouchableOpacity>
-                    <View style={styles.separator} />
-                    <TouchableOpacity
-                        onPress={this.register}
-                        style={styles.buttonStyle}>
-                        <Text style={styles.buttonText}>Zaregistrovať sa</Text>
-                    </TouchableOpacity>
-                </View>
+    
+    return (
+        <View style={styles.container}>
+            <Text style={styles.logo}>Autobazár</Text>
+            <View style={styles.buttonMargin}>
+                <TextInput style={styles.inputText} placeholder="Prihlasovacie meno" onChangeText={(text) => setEmail(text)} />
                 <View style={styles.separator} />
+                <TextInput style={styles.inputText} placeholder="Heslo" onChangeText={(text) => setPassword(text)} secureTextEntry={true} />
                 <View style={styles.separator} />
-                {this.state.isLoading && <ActivityIndicator size="large" color="#081c15" />}
-                <StatusBar style="auto" />
+                <TouchableOpacity
+                    onPress={login}
+                    style={styles.buttonStyle}>
+                    <Text style={styles.buttonText}>Prihlásiť sa</Text>
+                </TouchableOpacity>
+                <View style={styles.separator} />
+                <TouchableOpacity
+                    onPress={() => props.showRegister()}
+                    style={styles.buttonStyle}>
+                    <Text style={styles.buttonText}>Zaregistrovať sa</Text>
+                </TouchableOpacity>
             </View>
-        )
-    }
+            <View style={styles.separator} />
+            <View style={styles.separator} />
+            {isLoading && <ActivityIndicator size="large" color="#081c15" />}
+            <StatusBar style="auto" />
+        </View>
+    );
+    
 }
 
 const styles = StyleSheet.create({
