6e06ad44f4849f7290ee01ceb566616c1238608e
Daniel Cok <cok@asdata.sk>
2022-04-18 21:38:45 +0200

    Merge branch 'frontend-work-jv' of https://github.com/plugin23/autobazar-frontend into frontend-work-jv

--- components/AddCarScreen.js
+++ components/AddCarScreen.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect, Children } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, Alert, ActivityIndicator, TouchableOpacity} from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import { fetchAPI } from '../Api'
-<<<<<<< HEAD
-=======
-import * as ImagePicker from "expo-image-picker";
-import { storage } from './firebase';
-import fs from 'fs'
-import fetch from 'node-fetch'
-
-
-=======
 import { createStackNavigator } from '@react-navigation/stack'
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput, Alert, ActivityIndicator, TouchableOpacity, Dimensions } from 'react-native';
@@ -26,7 +7,6 @@ import * as ImagePicker from 'expo-image-picker';
 import uuid from 'uuid';
 import firebase from "firebase/app";
 import "firebase/storage";
->>>>>>> 826eeddb59b9f88daaa4dcb351577ec3bcf7bb51
 
 const firebaseConfig = {
     apiKey: "AIzaSyC3bvlOlY1gGFWUiSDMg9YA94E8hwGSwuo",
@@ -36,171 +16,19 @@ const firebaseConfig = {
     messagingSenderId: "276629828442",
     appId: "1:276629828442:web:876afc5961294466e47963",
     measurementId: "G-XL77BC1PLX"
-<<<<<<< HEAD
-  };
-  
-  // Initialize Firebase
-  //initializeApp(firebaseConfig);
-
-=======
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
->>>>>>> 826eeddb59b9f88daaa4dcb351577ec3bcf7bb51
->>>>>>> frontend-work-jv
 
 const Stack = createStackNavigator()
 
 const AddCarScreen = (props) => {
-<<<<<<< HEAD
-=======
 
->>>>>>> 826eeddb59b9f88daaa4dcb351577ec3bcf7bb51
     const [carBrand, setCarBrand] = useState("")
     const [engineCap, setEngineCap] = useState("")
     const [year, setYear] = useState(0)
     const [mileage, setMileage] = useState(0)
     const [price, setPrice] = useState(0)
-<<<<<<< HEAD
-    const [doors, seDdoors] = useState(0)
-    const [description, setdescription] = useState("")
-    const [body, setBody] = useState("") 
-    const [car_name, setCar_name] = useState("") 
-    const [car_model, setcar_model] = useState("") 
-    const [isAdded, setIsAdded] = useState(false)
-    const [car_id, setCarId] = useState("")
-
-
-    useEffect(() => { 
-        const fetchCars = () => {
-            //isFetching = true -> fetchapi -> setCars
-            setCars([])
-        }
-    }, [])
-
-
-    const addCar = () => {
-        if (carBrand == "" || engineCap == "" || year=="" || mileage=="" || price=="" || doors=="" || description=="" || body=="" || car_model=="" || car_name=="") {
-            Alert.alert("Prázdne pole", "Prosím vyplňte obe polia", [{ text: "OK", onPress: () => { } }])
-            return
-        }
-
-        const bodyObject = {
-            author: props.userId,
-            year: year,
-            mileage: mileage,
-            price: price,
-            doors: doors,
-            description: description,
-            engine_cap: engineCap,
-            car_brand: carBrand,
-            body: body,
-            car_name: car_name,
-            car_model : car_model
-            
-        }
-
-        fetchAPI('api/autobazar/cars', 'POST', bodyObject).then(result => {
-            
-            if (result._id) {
-                setIsAdded(true)
-                setCarId(result._id)
-                edit_user(result)
-                
-            }
-            else { //ak uzivatel zada zle heslo alebo meno
-                Alert.alert("Nesprávne údaje", "Údaje, ktoré ste zadali nie sú správne", [{ text: "OK", onPress: () => { } }])
-            }
-        })       
-    
-    }
-
-   
-
-    const edit_user = (result) => {
-        console.log(result._id)
-
-        const bodyObjectUser = {
-            own_advertisement: result._id 
-        }
-<<<<<<< HEAD
-        
-        console.log(bodyObjectUser)
-
-        fetchAPI(`api/autobazar/users/${result.author}/own_advertisement`, 'PUT', bodyObjectUser).then(result => {
-
-            if (result) {
-                alert('pridane aj userovi')                
-            }
-            else { //ak uzivatel zada zle heslo alebo meno
-                Alert.alert("Nepodarilo sa vložiť užívateľovi tento inzerát", "Údaje, ktoré ste zadali nie sú správne", [{ text: "OK", onPress: () => { } }])
-            }
-        })
-=======
-    };
-
-    const handleUpdate = () => {
-        const uploadTask = storage.ref(`images/$image.name`).put(image);
-        console.log(uploadTask)
-            uploadTask.on(
-                "status_changed",
-                snapshot => {},
-                () => {
-                    storage
-                    .ref("images")
-                    .child(image.name)
-                    .getDownloadURL()
-                    .then(url => {
-                        console.log('som tuaaaa')
-                        console.log(url);
-                        setUrl(url);
-                    });
-                }
-            )
-            console.log(uploadTask)    
->>>>>>> frontend-work-jv
-    }
-
-    return (
-        isAdded ? (
-            <h1>uspešne pridaný inzerát</h1>
-        )
-        :        
-        (<View style={styles.container}>
-            <h1>Pridať inzerát</h1>
-            <ScrollView style={styles.buttonMargin}>
-                <TextInput style={styles.inputText} placeholder="Značka" onChangeText={(text) => setCarBrand(text)} />
-                <View style={styles.separator} />                
-                <TextInput style={styles.inputText} placeholder="Model" onChangeText={(text) => setcar_model(text)} />
-                <View style={styles.separator} />
-                <TextInput style={styles.inputText} placeholder="Výkon" onChangeText={(text) => setEngineCap(text)} />
-                <View style={styles.separator} />
-                <TextInput style={styles.inputText} placeholder="Car name" onChangeText={(text) => setCar_name(text)} />
-                <View style={styles.separator} />
-                <TextInput style={styles.inputText} placeholder="Rok výroby" onChangeText={(text) => setYear(text)} />
-                <View style={styles.separator} />
-                <TextInput style={styles.inputText} placeholder="Počet  najazdenýchkilometrov" onChangeText={(text) => setMileage(text)} />
-                <View style={styles.separator} />
-                <TextInput style={styles.inputText} placeholder="Počet dverí" onChangeText={(text) => seDdoors(text)} />
-                <View style={styles.separator} />
-                <TextInput style={styles.inputText} placeholder="Karoséria" onChangeText={(text) => setBody(text)} />
-                <View style={styles.separator} />
-                <TextInput style={styles.inputText} placeholder="Cena" onChangeText={(text) => setPrice(text)} />
-                <View style={styles.separator} />
-                <TextInput style={styles.inputText} placeholder="Popis" onChangeText={(text) => setdescription(text)} />
-                <View style={styles.separator} />
-                <View style={{ paddingVertical: 10 }} />
-                <TouchableOpacity
-                    onPress={addCar}
-                    style={styles.buttonStyle}>
-                    <Text style={styles.buttonText}>Pridať inzerát</Text>
-                </TouchableOpacity>
-                <View style={styles.separator} />
-            </ScrollView>
-            <View style={styles.separator} />
-        </View>        
-)
-=======
     const [doors, setDoors] = useState(0)
     const [description, setDescription] = useState("")
     const [body, setBody] = useState("")
@@ -231,6 +59,7 @@ const AddCarScreen = (props) => {
 
                 if (result._id) {
                     setIsAdded(true)
+                    edit_user(result)
                 }
                 else {
                     Alert.alert("Údaje, ktoré ste zadali nie sú správne", [{ text: "OK", onPress: () => { } }])
@@ -281,6 +110,27 @@ const AddCarScreen = (props) => {
         return downloadURL
     }
 
+
+    const edit_user = (result) => {
+        console.log(result._id)
+
+        const bodyObjectUser = {
+            own_advertisement: result._id 
+        }
+        
+        console.log(bodyObjectUser)
+
+        fetchAPI(`api/autobazar/users/${result.author}/own_advertisement`, 'PUT', bodyObjectUser).then(result => {
+
+            if (result) {
+                alert('pridane aj userovi')                
+            }
+            else { //ak uzivatel zada zle heslo alebo meno
+                Alert.alert("Nepodarilo sa vložiť užívateľovi tento inzerát", "Údaje, ktoré ste zadali nie sú správne", [{ text: "OK", onPress: () => { } }])
+            }
+        })
+    }
+
     return (
         isAdded ? (
             <Text style={styles.logo}>Úspešne pridaný inzerát</Text>
@@ -321,27 +171,18 @@ const AddCarScreen = (props) => {
                     <View style={styles.separator} />
                 </KeyboardAwareScrollView>
             )
->>>>>>> 826eeddb59b9f88daaa4dcb351577ec3bcf7bb51
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-<<<<<<< HEAD
-        flex: 1,
-=======
         flexGrow: 1,
->>>>>>> 826eeddb59b9f88daaa4dcb351577ec3bcf7bb51
         backgroundColor: '#fff',
         justifyContent: 'center',
         alignItems: 'center',
     },
     separator: {
-<<<<<<< HEAD
-        marginVertical: 8
-=======
         marginVertical: 10
->>>>>>> 826eeddb59b9f88daaa4dcb351577ec3bcf7bb51
     },
     buttonStyle: {
         width: 300,
@@ -356,39 +197,20 @@ const styles = StyleSheet.create({
         color: '#fff'
     },
     inputText: {
-<<<<<<< HEAD
-        borderColor: "#b7e4c7",
-        borderWidth: 1,
-        borderRadius: 3,
-        paddingHorizontal: 10,
-=======
         borderColor: "#000",
         borderWidth: 1,
         borderRadius: 10,
         paddingHorizontal: 10,
         width: '80%',
         height: 50,
->>>>>>> 826eeddb59b9f88daaa4dcb351577ec3bcf7bb51
         paddingVertical: 8
     },
     logo: {
         fontSize: 50,
         textAlign: "center",
-<<<<<<< HEAD
-=======
         marginTop: 70,
->>>>>>> 826eeddb59b9f88daaa4dcb351577ec3bcf7bb51
         marginBottom: 50
     }
 });
 
-<<<<<<< HEAD
 export default AddCarScreen
-=======
-<<<<<<< HEAD
-export default AddCarScreen
->>>>>>> Stashed changes
-=======
-export default AddCarScreen
->>>>>>> 826eeddb59b9f88daaa4dcb351577ec3bcf7bb51
->>>>>>> frontend-work-jv
